55e5a02ba7c3d92f2a393a514ae58b435d6479d7
Juliette Knightley <knightleyjb@gmail.com>
2019-02-11 14:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var name = "Susan"
 var height = 39
 var message = ` is ${height} inches tall`
-module. exports = ${name} is ${height} inches tall`
+
+module. exports = {name, height, message}
  
  
 
